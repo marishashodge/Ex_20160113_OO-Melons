be12258e3e283c80f3580e1819c4751b85c8ba45
Hackbright Student <info@hackbrightacademy.com>
2016-01-14 17:15:11 -0800

    working version that does all the functions (but is redundant)

---
+++ melons.py
@@ -0,0 +1,56 @@
+"""This file should have our order classes in it."""
+
+class AbstractMelonOrder(object):
+ 
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+       
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = "international"
+        self.tax = 0.17
+        self.country_code = country_code
+
+    def get_country_code(self):
+        """Return the country code."""
+        return self.country_code
+
+   
+    
+
+    # def __init__(self, country_code):
+    #     self.country_code = country_code
+
+
